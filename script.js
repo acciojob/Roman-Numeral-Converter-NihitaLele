9383683c94615ae441aeb3c402c4adecfd57d559
Nihita Lele <124602601+NihitaLele@users.noreply.github.com>
2024-03-06 18:50:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
+    const obj = {
+        0: ['M', 1000],
+        1: ['D', 500],
+        2: ['C', 100],
+        3: ['L', 50],
+        4: ['X', 10],
+        5: ['V', 5],
+        6: ['I', 1]
     };
 
-  //your code here
+    let romanNumeral = '';
 
+    for (let key in obj) {
+        let value = obj[key][1];
+        let symbol = obj[key][0];
+
+        while (num >= value) {
+            romanNumeral += symbol;
+            num -= value;
+        }
+
+        // Check for subtractive notation
+        if (key < 6 && num >= obj[key][1] - obj[key + 2][1] && obj[key + 2][1] !== 1000) {
+            romanNumeral += obj[key + 2][0] + obj[key][0];
+            num -= (obj[key][1] - obj[key + 2][1]);
+        }
+    }
+
+    return romanNumeral;
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
